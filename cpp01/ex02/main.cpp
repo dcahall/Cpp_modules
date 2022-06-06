0e280e8f77c39153a71c7d39e2b2321aca2ca460
Dababi Cahall <dcahall@pr-l4.kzn.21-school.ru>
2022-06-06 21:33:39 +0300

    simple task with references

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcahall <dcahall@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/06 21:22:45 by dcahall           #+#    #+#             */
+/*   Updated: 2022/06/06 21:32:53 by dcahall          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(void){
+	std::string	str = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &str;
+	std::string	&stringREF = str;
+
+	std::cout << "Memory address" << std::endl;
+	std::cout << "str: " << &str << std::endl;
+	std::cout << "string PTR: " << &stringPTR << std::endl;
+	std::cout << "stringREF: " << &stringREF << std::endl;
+
+	std::cout << "value in variable" << std::endl;
+	std::cout << "str: " << str << std::endl;
+	std::cout << "string PTR: " << *stringPTR << std::endl;
+	std::cout << "stringREF: " << stringREF << std::endl;
+}
