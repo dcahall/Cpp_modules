16dbd7e16c04a585bf60cf18ad6a4ad0384d5dc5
Dababi Cahall <dcahall@pr-c5.kzn.21-school.ru>
2022-06-22 11:24:52 +0300

    minor changes

--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dcahall <dcahall@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 17:25:00 by dcahall           #+#    #+#             */
-/*   Updated: 2022/06/17 12:03:26 by dcahall          ###   ########.fr       */
+/*   Updated: 2022/06/22 11:24:16 by dcahall          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,16 @@ int main(void){
 	}
 	try
 	{
-		std::cout << Bureaucrat("Random", 30) << std::endl;;
+		std::cout << Bureaucrat("Random", 30) << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << e.what() << '\n';
+	}
+	try
+	{
+		Bureaucrat tmp = Bureaucrat("Random", 150);
+		tmp.decrementGrade();
 	}
 	catch(const std::exception& e)
 	{
